5a99e9cd53445ca4a7222ef9ff6eeeca2bc8ee24
sebi37 <166393086+sebi37@users.noreply.github.com>
2024-04-29 09:19:13 +0200

    Update Main.java useless comment eingefügt !!

--- sebi37_Hubert005_android/DB_Test/src/Main.java
+++ sebi37_Hubert005_android/DB_Test/src/Main.java
@@ -4,7 +4,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
+// this is just a useless comment *****************
 public class Main {
 
 	private static int id;
